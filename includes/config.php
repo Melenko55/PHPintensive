c56123c556adce87d134337393c8e3c6e289726b
Melya <melenko55@gmail.com>
2021-11-12 23:40:27 +0200

    First day

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+define('SITE_NAME', 'Twitter');
+// . - конкатенация строк
+// $_SERVER['HTTP_HOST'] - получение домена
+define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'twitter');
+define('DB_USER', 'root'); 
+define('DB_PASS', ''); 
